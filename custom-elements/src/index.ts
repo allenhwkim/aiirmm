82a0571ae16cb735d44b800206ae8ebaf6d15336
allen kim <allenkim@allens-MacBook-Air.local>
2023-03-05 10:53:55 -0500

    updated

--- custom-elements/src/index.ts
+++ custom-elements/src/index.ts
@@ -1,4 +1,4 @@
-import FormflowChart from "./reactflow/FormflowChart"
+import FormflowChart from "./reactflow/FormflowChart/FormflowChart"
 import FormflowChartElement from './form-diagram/form-diagram';
 
 export {FormflowChart, FormflowChartElement};
