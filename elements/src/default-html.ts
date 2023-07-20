1ff8e598adbc5534cb0b9daebfb3eefef275a696
Allen Kim <allen.kim@ontariohealth.ca>
2023-07-20 17:15:43 -0400

    updated

---
+++ elements/src/default-html.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_HTML = /*html*/ `
+  <div class="p-3">
+    <form-stepper class="pb-4"></form-stepper> 
+    <div class="form-flow form-errors error"></div>
+    <form class="form-flow p-2"></form>
+    <div class="form-flow form-buttons container">
+      <button class="btn btn-primary me-2 form-prev">Prev</button>
+      <button class="btn btn-primary me-2 form-next">Next</button>
+      <button class="btn btn-primary me-2 form-review">Review</button>
+      <button class="btn btn-primary me-2 form-submit">Submit</button>
+    </div>
+  </div>
+`;
+      
