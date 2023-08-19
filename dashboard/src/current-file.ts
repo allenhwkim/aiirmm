eb2b9f94f0a69d4739086e7673a47fb8a29f308a
Kim, Allen <allen.kim@ontariohealth.ca>
2023-08-19 13:04:42 -0400

    updated

--- dashboard/src/current-file.ts
+++ dashboard/src/current-file.ts
@@ -5,14 +5,20 @@ import type { Node, Edge } from 'reactflow';
 const DEFAULT_CHART =  {
   nodes:[
     {id: 'start', type: 'start', deletable: false, position: { x: 100, y: 0 }},
-    {id: 'page1', type: 'custom', data: {label: 'page1'}, position: { x: 100, y: 87 }},
-    {id: 'review', type: 'custom', data: {label: 'review'}, position: { x: 100, y: 187 }},
-    {id: 'submit', type: 'custom', data: {label: 'submit', type: 'submit'}, position: { x: 100, y: 287 }},
-    {id: 'end', type: 'end', deletable: false, position: { x: 100, y: 400 }},
+    {id: 'page1', type: 'custom', data: {label: 'page1'}, position: { x: -150, y: 150 }},
+    {id: 'page2', type: 'custom', data: {label: 'page2'}, position: { x: 100, y: 150 }},
+    {id: 'page3', type: 'custom', data: {label: 'page3'}, position: { x: 350, y: 150 }},
+    {id: 'review', type: 'custom', data: {label: 'review'}, position: { x: 100, y: 300 }},
+    {id: 'submit', type: 'custom', data: {label: 'submit', type: 'submit'}, position: { x: 100, y: 450 }},
+    {id: 'end', type: 'end', deletable: false, position: { x: 100, y: 600 }},
   ] as Node[],
   edges: [
-    {id: 'start-page1', source: 'start', target: 'page1', type: 'custom'},
-    {id: 'page1-review', source: 'page1', target: 'review', type: 'custom', data: {hello: 'world'}},
+    {id: 'start-page1', source: 'start', target: 'page1', type: 'custom', label: 'if weekday'},
+    {id: 'start-page2', source: 'start', target: 'page2', type: 'custom', label: 'if Saturday'},
+    {id: 'start-page3', source: 'start', target: 'page3', type: 'custom', label: 'if Sunday'},
+    {id: 'page1-review', source: 'page1', target: 'review', type: 'custom'},
+    {id: 'page2-review', source: 'page2', target: 'review', type: 'custom'},
+    {id: 'page3-review', source: 'page3', target: 'review', type: 'custom'},
     {id: 'review-submit', source: 'review', target: 'submit', type: 'custom'},
     {id: 'submit-end', source: 'submit', target: 'end', type: 'custom'},
   ] as Edge[]
